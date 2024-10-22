d700452d9662d348144a539122b3bddd375a07b2
Alisson Martin <alissonmartin2011@hotmail.com>
2024-10-21 23:33:52 -0300

    Fix: fixing tests

--- src/main/java/com/github/AlissonMartin/ong/dtos/UpdateUserRequestDTO.java
+++ src/main/java/com/github/AlissonMartin/ong/dtos/UpdateUserRequestDTO.java
@@ -1,6 +1,7 @@
 package com.github.AlissonMartin.ong.dtos;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.springframework.web.multipart.MultipartFile;
 
-public record UpdateUserRequestDTO(String name, String password, String federal_tax_id, MultipartFile photo) {
+public record UpdateUserRequestDTO(String name, String password, String federalTaxId, @JsonIgnore MultipartFile photo) {
 }
