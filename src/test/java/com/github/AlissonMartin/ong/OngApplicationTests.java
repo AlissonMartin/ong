9d0530c80712827a983732b6d350c9b21acf8982
Alisson <alissonmartin2011@hotmail.com>
2024-10-22 18:20:08 -0300

    Config: Setting h2 db for tests

--- src/test/java/com/github/AlissonMartin/ong/OngApplicationTests.java
+++ src/test/java/com/github/AlissonMartin/ong/OngApplicationTests.java
@@ -2,8 +2,10 @@ package com.github.AlissonMartin.ong;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class OngApplicationTests {
 
 	@Test
