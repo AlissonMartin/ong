9f11e7e4be902b88e28805fb2b801bf7c6699c0e
Alisson <alissonmartin2011@hotmail.com>
2025-06-23 23:58:06 -0300

    Config: Fixing cors

--- src/main/java/com/github/AlissonMartin/ong/config/security/WebConfig.java
+++ src/main/java/com/github/AlissonMartin/ong/config/security/WebConfig.java
@@ -13,6 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
     registry.addMapping("/**")
             .allowedOriginPatterns("*")
             .allowedMethods("*")
-            .allowedHeaders("*");
+            .allowedHeaders("*")
+            .allowCredentials(true);
   }
 }
