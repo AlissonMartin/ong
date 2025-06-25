9e48b7f5814713692824749840cf555a70b885d7
Alisson <alissonmartin2011@hotmail.com>
2025-06-25 18:47:21 -0300

    Fix: Adding getters and setters to city and state

--- src/main/java/com/github/AlissonMartin/ong/models/State.java
+++ src/main/java/com/github/AlissonMartin/ong/models/State.java
@@ -1,11 +1,15 @@
 package com.github.AlissonMartin.ong.models;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.Set;
 
 @Entity
 @Table(name = "states")
+@Getter
+@Setter
 public class State {
 
     @Id
